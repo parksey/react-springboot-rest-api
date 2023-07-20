af4d5bb36c318a9bc7253f65aa3ecaa1a5caeb38
parksey <parkseyeon99@naver.com>
2023-07-21 00:25:12 +0900

    test: Customer테스트 코드 추가

---
+++ be/src/test/java/com/example/gonggam/customer/service/CustomerTest.java
@@ -0,0 +1,52 @@
+package com.example.gonggam.customer.service;
+
+import com.example.gonggam.customer.domain.Customer;
+import com.example.gonggam.owner.domain.Owner;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("Customer테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CustomerTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void init() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    void 회원_생성_성공_테스트() {
+        // Given
+        String name = "psy";
+        String phone = "01012345678";
+        String email = "psy@naver.com";
+
+        Customer customer = Customer.builder()
+                .email(email)
+                .phone(phone)
+                .name(name)
+                .password("1234")
+                .build();
+
+        // When
+        Set<ConstraintViolation<Customer>> validates = validator.validate(customer);
+
+        // Then
+        assertThat(validates).isNotNull();
+        assertAll(
+                () -> assertThat(customer.getName()).isEqualTo(name),
+                () -> assertThat(customer.getEmail()).isEqualTo(email),
+                () -> assertThat(customer.getPhone()).isEqualTo(phone)
+        );
+    }
+}
