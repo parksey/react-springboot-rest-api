bde050d9311e2ed6011113a9063d08b5f6eb7d8d
parksey <parkseyeon99@naver.com>
2023-07-20 19:30:09 +0900

    feat: 스키마 데이터 추가 및 test 컨테이너 정보 변경

--- docker/db-init/schema.sql
+++ docker/db-init/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS owner;
-CREATE TABLE owner (
+CREATE TABLE owner
+(
     owner_id    BIGINT       AUTO_INCREMENT,
     owner_no    VARCHAR(10)  NOT NULL,
     phone       VARCHAR(11)  NOT NULL ,
@@ -8,3 +9,29 @@ CREATE TABLE owner (
     CONSTRAINT uk_owner_no UNIQUE(owner_no),
     CONSTRAINT pk_owner_id PRIMARY KEY (owner_id)
 );
+
+INSERT INTO owner (owner_no, phone, email)
+VALUES
+    ('1234568790', '01011112222', 'psy@naver.com'),
+    ('4567890123', '01014725836', 'psy2@naver.com');
+
+DROP TABLE IF EXISTS shared_space;
+CREATE TABLE shared_space
+(
+    space_id    BIGINT      AUTO_INCREMENT,
+    title       VARCHAR(50) NOT NULL,
+    description TEXT,
+    location    VARCHAR(100) NOT NULL,
+    capacity    INTEGER      NOT NULL,
+    amount      BIGINT       NOT NULL,
+    start_at    TIMESTAMP    NOT NULL,
+    end_at      TIMESTAMP    NOT NULL,
+    owner_id    BIGINT       NOT NULL,
+
+    PRIMARY KEY (space_id)
+);
+
+INSERT INTO shared_space (title, description, location, capacity, amount, start_at, end_at, owner_id)
+VALUES
+    ('title1', '설명1', '서울특별시', 5, 29000, '2023-07-23 13:00', '2023-07-25 18:00', 1),
+    ('제목', '설명2', '인천', 90, 10000, '2023-07-23 13:00', '2023-07-25 18:00', 2);
