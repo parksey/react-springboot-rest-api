fa3c73e79799fc113dc24fd8e5f978e54ed25ec9
parksey <parkseyeon99@naver.com>
2023-07-20 16:32:22 +0900

    test: SharedSpace테스트 코드 추가

--- be/src/main/java/com/example/gonggam/space/repository/SharedSpaceRepository.java
+++ be/src/main/java/com/example/gonggam/space/repository/SharedSpaceRepository.java
@@ -2,10 +2,17 @@ package com.example.gonggam.space.repository;
 
 import com.example.gonggam.space.domain.SharedSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SharedSpaceRepository extends JpaRepository<SharedSpace, Long> {
 
-    List<SharedSpace> findAllByOwnerNo(String ownerNo);
+    @Query("SELECT a " +
+            "FROM SharedSpace as a " +
+            "JOIN Owner as b " +
+              "ON a.ownerId = b.ownerId " +
+           "WHERE b.ownerNo = :ownerNo")
+    List<SharedSpace> findAllByOwnerNo(@Param(value = "ownerNo") String ownerNo);
 }
--- be/src/test/java/com/example/gonggam/space/SharedSpaceRepositoryTest.java
+++ be/src/test/java/com/example/gonggam/space/SharedSpaceRepositoryTest.java
@@ -2,13 +2,52 @@ package com.example.gonggam.space;
 
 
 import com.example.gonggam.global.testconfig.RepositoryAnnotation;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
+import com.example.gonggam.space.domain.SharedSpace;
+import com.example.gonggam.space.repository.SharedSpaceRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("SharedSpaceRepository 테스트")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @ActiveProfiles("test")
-public class SharedSpaceRepositoryTest extends RepositoryAnnotation {
+class SharedSpaceRepositoryTest extends RepositoryAnnotation {
+
+    @Autowired
+    private SharedSpaceRepository sharedSpaceRepository;
+
+    private static SharedSpace sharedSpace;
+
+    @BeforeAll
+    static void init() {
+        // Given
+        sharedSpace = SharedSpace.builder()
+                .title("title")
+                .amount(29000)
+                .location("서울특별시")
+                .capacity(7)
+                .startAt(LocalDateTime.now())
+                .endAt(LocalDateTime.now().plusDays(7))
+                .ownerId(1L)
+                .build();
+    }
+
+    @Test
+    void 공유공간_생성_성공_테스트() {
+         // When
+        SharedSpace savedSharedSpace = sharedSpaceRepository.save(sharedSpace);
+
+        // Then
+        assertThat(savedSharedSpace.getSpaceId()).isNotNull();
+        assertAll(
+                () -> assertThat(savedSharedSpace.getTitle()).isEqualTo(sharedSpace.getTitle()),
+                () -> assertThat(savedSharedSpace.getStartAt()).isEqualTo(sharedSpace.getStartAt()),
+                () -> assertThat(savedSharedSpace.getEndAt()).isEqualTo(sharedSpace.getEndAt())
+        );
+    }
 }
