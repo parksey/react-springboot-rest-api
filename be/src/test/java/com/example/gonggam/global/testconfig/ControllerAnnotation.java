bb15911b22d687a1ae3fa9697179e01445d15daf
parksey <parkseyeon99@naver.com>
2023-07-20 19:32:43 +0900

    refactor: 공통 Controller test관련 의존성 상위 클래스로 변경

--- be/src/test/java/com/example/gonggam/global/testconfig/ControllerAnnotation.java
+++ be/src/test/java/com/example/gonggam/global/testconfig/ControllerAnnotation.java
@@ -1,12 +1,27 @@
 package com.example.gonggam.global.testconfig;
 
 import com.example.gonggam.GonggamApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest(classes = GonggamApplication.class)
 @AutoConfigureMockMvc
-public interface ControllerAnnotation{
+public class ControllerAnnotation{
+
+    private static String API_VERSION = "/api/v1";
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected String getUrl(String url) {
+        return API_VERSION + url;
+    }
 }
