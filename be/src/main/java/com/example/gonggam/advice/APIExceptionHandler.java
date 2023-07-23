c0613b53bd2936505ad88723d07828123f8552e3
parksey <parkseyeon99@naver.com>
2023-07-23 20:00:24 +0900

    feat: 예외 advice 컨트롤러 기능 추가 및 반환 객체 클래스 추가

---
+++ be/src/main/java/com/example/gonggam/advice/APIExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.example.gonggam.advice;
+
+import com.example.gonggam.customer.exception.CustomerException;
+import com.example.gonggam.owner.exception.OwnerException;
+import com.example.gonggam.reservation.exception.ReservationException;
+import com.example.gonggam.space.exception.SharedSpaceException;
+import com.example.gonggam.util.exception.CustomException;
+import com.example.gonggam.util.exception.CustomValidationStatus;
+import org.hibernate.SessionException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class APIExceptionHandler {
+
+    @ExceptionHandler({CustomerException.class,
+            OwnerException.class,
+            ReservationException.class,
+            SharedSpaceException.class,
+            SessionException.class})
+    public ResponseEntity<ErrorResponse> handleVoucherException(CustomException exception) {
+        return handleExceptionInternal(exception.getValidationStatus());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<ErrorResponse> handleGlobalException(RuntimeException runtimeException) {
+        return handleExceptionInternal(HttpStatus.NOT_FOUND, runtimeException.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(CustomValidationStatus exceptionCode) {
+        return ResponseEntity.status(exceptionCode.getHttpStatus())
+                .body(errorResponse(exceptionCode.name(), exceptionCode.getMessage()));
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(HttpStatus httpStatus, String message) {
+        return ResponseEntity.status(httpStatus)
+                .body(errorResponse(httpStatus.getReasonPhrase(), message));
+    }
+
+    private ErrorResponse errorResponse(String code, String message) {
+        return new ErrorResponse(code, message);
+    }
+}
